f887cb2eb9648ebc15bb728c7ab14f9878159b95
Graves067 <c.a.gillespie9@gmail.com>
2025-02-12 00:16:40 -0800

    ball follows char

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -4,6 +4,7 @@ using UnityEngine.Events;
  public class BallController : MonoBehaviour{
 
     [SerializeField] private float force = 1f;
+    [SerializeField] private Transform ballAnchor;
     [SerializeField] private InputManager inputManager;
     private bool isBallLaunched;
     private Rigidbody ballRB;
@@ -12,13 +13,18 @@ void Start(){
 
     ballRB = GetComponent<Rigidbody>();
     inputManager.OnSpacePressed.AddListener(LaunchBall);
+        transform.parent = ballAnchor;
+        transform.localPosition = Vector3.zero;
+        ballRB.isKinematic = true;
 
     }
 
-private void LaunchBall(){
+    private void LaunchBall(){
 
         if (isBallLaunched) return;
         isBallLaunched = true;
+        transform.parent = null;
+        ballRB.isKinematic = false;
         ballRB.AddForce(transform.forward * force, ForceMode.Impulse);
   }
 }
