eb1528a561957eb986c1fd96cacec92cb0735ad4
Graves067 <c.a.gillespie9@gmail.com>
2025-02-11 23:26:43 -0800

    made gutters

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -2,21 +2,21 @@ using UnityEngine;
 using UnityEngine.Events;
 
  public class BallController : MonoBehaviour{
-[SerializeField] private float force = 1f;
-    private InputManager inputManager;
-    private bool isBallLaunched = false;
+
+    [SerializeField] private float force = 1f;
+    [SerializeField] private InputManager inputManager;
+    private bool isBallLaunched;
     private Rigidbody ballRB;
 
 void Start(){
-ballRB = GetComponent<Rigidbody>();
 
- // Add a listener to the OnSpacePressed event.
-// When the space key is pressed the
-// LaunchBall method will be called.
-inputManager.OnSpacePressed.AddListener(LaunchBall);
+    ballRB = GetComponent<Rigidbody>();
+    inputManager.OnSpacePressed.AddListener(LaunchBall);
+
     }
 
 private void LaunchBall(){
+
         if (isBallLaunched) return;
         isBallLaunched = true;
         ballRB.AddForce(transform.forward * force, ForceMode.Impulse);
