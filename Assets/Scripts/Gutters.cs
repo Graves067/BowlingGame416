568e7d9fd776e8185fc32f8d9c8112ded3a01f7e
Graves067 <c.a.gillespie9@gmail.com>
2025-02-15 14:53:17 -0800

    fixed gutters

--- Assets/Scripts/Gutters.cs
+++ Assets/Scripts/Gutters.cs
@@ -10,6 +10,6 @@ public class Gutters : MonoBehaviour
         ballRigidBody.linearVelocity = Vector3.zero;
         ballRigidBody.angularVelocity = Vector3.zero;
 
-        ballRigidBody.AddForce(transform.forward * velocityMagnitude, ForceMode.VelocityChange);
+        ballRigidBody.AddForce(transform.up * velocityMagnitude, ForceMode.VelocityChange);
     }
 }
